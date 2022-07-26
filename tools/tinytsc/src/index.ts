8496d427f1524a32e183e06de125803f62884bcb
den <denvstheworld@gmail.com>
2022-07-26 19:40:02 -0400

    Update tinytsc

--- tools/tinytsc/src/index.ts
+++ tools/tinytsc/src/index.ts
@@ -2,10 +2,11 @@
 
 import { program } from 'commander';
 import path from 'path';
-import { doBuild } from './compiler';
+import { doBuild, doCopy } from './compiler';
 
 program.parse(process.argv);
 
 let og: string = path.resolve(process.cwd());
 
 doBuild(og);
+doCopy(og);
